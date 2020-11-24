794724088773a60d9422783d3d2beb26c267e0d4
Ketsia-a <iraketsia2001@gmail.com>
2020-11-24 05:24:50 +0200

    added functions to create,save and display credentials

--- run.py
+++ run.py
@@ -21,3 +21,23 @@ def delete_users(user):
     Function to delete a user
     """
     user.delete_user()
+
+
+def create_credentials(account,username,password):
+    """
+    Function to create a new credential
+    """
+    new_credential = Credential(account,username,password)
+    return new_credential 
+
+def save_credentials(credential):
+    """
+    Function to save credential
+    """
+    redentials.save_credential()
+
+def display_all_credential():
+    """
+    Function that displays all credentials
+    """
+    return Credentials.display_all_credential()
