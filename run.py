534158d64c5fb97eb26b51bd130a351e9adada91
Ketsia-a <iraketsia2001@gmail.com>
2020-11-24 05:13:58 +0200

    made functions to create and save a user

---
+++ run.py
@@ -0,0 +1,22 @@
+from user import User
+from credentials import Credentials
+
+def create_users(username,password):
+    """
+    Function to create a new user
+    """
+    new_user = User(username,password)
+    return new_user
+
+
+def save_users(user):
+    """
+    Function to save user
+    """
+    user.save_user()
+
+def display_users(user):
+    """
+    Function to display saved user
+    """
+    return User.display_user()
